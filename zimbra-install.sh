17e60375819d6b1931a52db650afa06759b082aa
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2019-02-21 08:59:56 +0700

    Update zimbra-install.sh

--- zimbra-install.sh
+++ zimbra-install.sh
@@ -2,7 +2,7 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker pull jorgedlcruz/zimbra
-docker run -p 25:25 -p 80:80 -p 465:465 -p 587:587 -p 110:110 -p 143:143 -p 993:993 -p 995:995 -p 443:443 -p 8080:8080 -p 8443:8443 -p 7071:7071 -p 9071:9071 -h zimbra-docker.zimbra.io --dns 127.0.0.1 --dns 8.8.8.8 -i -t -e PASSWORD=Zimbra@Tenten jorgedlcruz/zimbra
+docker run -p 25:25 -p 80:80 -p 465:465 -p 587:587 -p 110:110 -p 143:143 -p 993:993 -p 995:995 -p 443:443 -p 8080:8080 -p 8443:8443 -p 7071:7071 -p 9071:9071 -h zimbra001-mtd.manhtuong.net --dns 127.0.0.1 --dns 8.8.8.8 -i -t -e PASSWORD=Zimbra@Tenten jorgedlcruz/zimbra
 IP="$(curl ifconfig.co)"
 echo "Cai Dat zimbra Thanh Cong "
 echo "Web Client - https://$IP"
