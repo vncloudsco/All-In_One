f8df86cb27f0a35370df1742e681c533180d4f82
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2019-02-27 16:30:53 +0700

    Update install.sh

--- openvpn/install.sh
+++ openvpn/install.sh
@@ -8,6 +8,14 @@ wget http://swupdate.openvpn.org/as/openvpn-as-2.0.26-CentOS$VS.x86_64.rpm
 yum localinstall openvpn-as-2.0.26-CentOS$VS.x86_64.rpm -y
 echo "openvpn:$PASD" | chpasswd
 clear
+echo "ban dang dung Openvpn day la thong tin dang nhap" >> /etc/motd
+echo "Admin Dang Nhap https://$IP:943/admin" >> /etc/motd
+echo "Dang Nhap https://$IP:943/" >> /etc/motd
+echo "Tai Khoan Dang Nhap la: openvpn" >> /etc/motd
+echo "mat khau dang nhap la: $PASD" >> /etc/motd
+
+
+
 echo "Admin Dang Nhap https://$IP:943/admin"
 echo "Dang Nhap https://$IP:943/"
 echo "Tai Khoan Dang Nhap la: openvpn"
