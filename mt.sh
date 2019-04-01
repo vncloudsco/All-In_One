fd3ef179816b27960b2326c0547c2d32f078a16e
vncloudsco <support@vnclouds.edu.vn>
2019-04-01 15:29:45 +0700

    Update mt.sh

--- mt.sh
+++ mt.sh
@@ -297,7 +297,7 @@ elif [ -f /etc/redhat-release ]; then
 		;;
 
 		11 ) 
-			curl -L -o one-click-installer.sh https://autoinstall.plesk.com/one-click-installer
+			curl -O https://autoinstall.plesk.com/one-click-installer
 			sh one-click-installer.sh
 			;;
 		12 ) exit 0;;
