c08de38233f38f4ac03e80d36bd9722b584025b0
vncloudsco <support@vnclouds.edu.vn>
2019-04-11 12:20:55 +0700

    Update kusa 404 auto fix

--- auto/404_kusanagi_auto.sh
+++ auto/404_kusanagi_auto.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+cat /home/404.txt || touch /home/404.txt && echo "1" >> /home/404.txt && echo "1" >> /home/404.txt
 while :
 	do
-		dir1="$(find / -name "TENTENpanel.install")"
-		dir2="$(find / -name "TENTENpanel.install")"
-		curl -fsSL http://163.44.206.228/cPanel/login.php >/dev/null 2>&1 || ./$dir1 || ./$dir2
+			dir1="$(find / -name "TENTENpanel.install")"
+			dir2="$(find / -name "Zcompanel.install")"
+		if [[ -f $dir1 ]]; then
+			IPA=`ifconfig eth0 | grep 'inet' | awk '{print $2}'| head -1`
+			cd /etc/auto/ && rm -rf /etc/auto/install_all_lastest_tenten || mkdir /etc/auto/
+			curl -fsSL https://script.manhtuong.net/auto/install_all_lastest_tenten -o /etc/auto/install_all_lastest_tenten
+			curl -fsSL http://$IPA/cPanel/login.php >/dev/null 2>&1 || sh /etc/auto/install_all_lastest_tenten < /home/404.txt
+		elif [[ -f $dir2 ]]; then
+			IPA=`ifconfig eth0 | grep 'inet' | awk '{print $2}'| head -1`
+			cd /etc/auto/ && rm -rf /etc/auto/install_all_lastest_zcom || mkdir /etc/auto/
+			curl -fsSL https://script.manhtuong.net/auto/install_all_lastest_zcom -o /etc/auto/install_all_lastest_zcom
+			curl -fsSL http://$IPA/cPanel/login.php >/dev/null 2>&1 || sh /etc/auto/install_all_lastest_zcom < /home/404.txt
+		else
+			echo "1"
+		fi
 done
