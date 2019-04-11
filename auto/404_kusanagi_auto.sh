cfbbd38903539f80e3823a34a2a66484a382d555
vncloudsco <support@vnclouds.edu.vn>
2019-04-11 14:53:32 +0700

    Update 404_kusanagi_auto.sh

--- auto/404_kusanagi_auto.sh
+++ auto/404_kusanagi_auto.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
-crontab -r
 cat /home/404.txt || touch /home/404.txt && echo "1" >> /home/404.txt && echo "1" >> /home/404.txt
+
+# function check
+tenten() {
+	  cd /tmp
+       wget -O cPanel.zip kusanagi.tenten.cloud/cPanelInstall/cPanel_version1.1.zip
+	   rm -rf cPanel
+	   mkdir -p /usr/share/cPanel
+	   mv cPanel.zip /usr/share/cPanel/
+       cd /usr/share/cPanel/
+	   unzip -o cPanel.zip
+}
+
+
+zcom() {
+
+	  cd /tmp
+       wget -O cPanel.zip 150.95.105.165/cPanelInstallZcom/cPanellastest.zip 
+	   rm -rf cPanel
+	   mkdir -p /usr/share/cPanel
+	   mv cPanel.zip /usr/share/cPanel/
+       cd /usr/share/cPanel/
+	   unzip -o cPanel.zip
+}
+
+# run game
+crontab -r
 while :
 	do
 			dir1="$(find / -name "TENTENpanel.install")"
 			dir2="$(find / -name "Zcompanel.install")"
 		if [[ -f $dir1 ]]; then
-			IPA=`ifconfig eth0 | grep 'inet' | awk '{print $2}'| head -1`
-			cd /etc/auto/ && rm -rf /etc/auto/install_all_lastest_tenten || mkdir /etc/auto/
-			curl -fsSL https://script.manhtuong.net/auto/install_all_lastest_tenten -o /etc/auto/install_all_lastest_tenten
-			curl -fsSL http://$IPA/cPanel/login.php >/dev/null 2>&1 || sh /etc/auto/install_all_lastest_tenten < /home/404.txt
+			tenten
 		elif [[ -f $dir2 ]]; then
-			IPA=`ifconfig eth0 | grep 'inet' | awk '{print $2}'| head -1`
-			cd /etc/auto/ && rm -rf /etc/auto/install_all_lastest_zcom || mkdir /etc/auto/
-			curl -fsSL https://script.manhtuong.net/auto/install_all_lastest_zcom -o /etc/auto/install_all_lastest_zcom
-			curl -fsSL http://$IPA/cPanel/login.php >/dev/null 2>&1 || sh /etc/auto/install_all_lastest_zcom < /home/404.txt
+			zcom
 		else
 			echo "1"
 		fi
