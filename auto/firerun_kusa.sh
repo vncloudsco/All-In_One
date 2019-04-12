374be67a873c9dabed3332ba76ec2f7efc55b959
vncloudsco <support@vnclouds.edu.vn>
2019-04-12 10:52:57 +0700

    Update firerun_kusa.sh

--- auto/firerun_kusa.sh
+++ auto/firerun_kusa.sh
@@ -16,9 +16,28 @@ while :
 		IPA=`ifconfig eth0 | grep 'inet' | awk '{print $2}'| head -1`
 		IS="$(curl http://$IPA/filerun/ 2>&1 | grep 403 | sed 's/[^0-9]*//g' | sed -n 1p)"
 		if [[ "$IS" == "403" ]]; then
-			mkdir -p /usr/share/filerun
-			cd /usr/share/filerun
-			curl -o Filerun.zip https://kusanagi.tenten.cloud/cPanelInstall/Filerun.zip
-			unzip Filerun.zip
-		fi
+
+			DB_ROOT_PASS=`cat /etc/motd | grep Pass | awk {'print $3'} | tail -n 1`
+			DB_PASS=`cat /etc/motd | grep Pass | awk {'print $3'} | tail -n 1`
+			DIR_ROOT=/usr/share/
+			rm -rf $DIR_ROOT/filerun
+			wget -O Filerun_db.sql kusanagi.tenten.cloud/cPanelInstall/Filerun_db_lastest.sql
+			mysql -uroot -p$DB_ROOT_PASS -e	"DROP DATABASE IF EXISTS Filerun_db"	
+			mysql -uroot -p$DB_ROOT_PASS -e "create database Filerun_db"
+			mysql -uroot -p$DB_ROOT_PASS -e "grant all privileges on Filerun_db.* to 'Filerun_user'@'%' identified by '$DB_PASS'"
+			mysql -uroot -p$DB_ROOT_PASS -e "flush privileges"
+			mysql -uroot -p$DB_ROOT_PASS -h localhost Filerun_db  < Filerun_db.sql
+			# download source zip file
+			cd $DIR_ROOT
+			mkdir -p filerun
+			cd filerun
+			wget -O Filerun.zip kusanagi.tenten.cloud/cPanelInstall/Filerun.zip 
+			unzip -o Filerun.zip
+			#wget -O unzip.php http://f.afian.se/wl/?id=HS&filename=unzip.php&forceSave=1
+			sed -i "s|100faf1c|$DB_PASS|" /usr/share/filerun/system/data/autoconfig.php
+			cd ..
+			chown -R kusanagi:kusanagi filerun/
+			chmod -R 755 filerun/
+	
+	fi
 done
