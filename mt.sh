1092654d3adc50c47af2775cdcec381c4a2e732b
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2019-05-07 11:06:09 +0700

    Update mt.sh

--- mt.sh
+++ mt.sh
@@ -68,7 +68,7 @@ if [ -f /etc/lsb-release ]; then
 			3 ) 
 			echo "tien hanh cai dat zimbra"
 			wget https://script.manhtuong.net/zimbra-install.sh
-			chomd +x zimbra-install.sh
+			chmod +x zimbra-install.sh
 			./zimbra-install.sh
 			rm -f zimbra-install.sh
 			;;
